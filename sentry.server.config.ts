790e156fb617ed8718403f47d311fb0c5329b75d
ankush <ankush@compose.co.in>
2024-09-07 19:16:24 +0530

    feat: integrate aceternity components

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://426d99c950568f7c1c7f8a8f11940284@o4507911653949440.ingest.de.sentry.io/4507911655850064",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
