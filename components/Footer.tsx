d1472603d22b97af6b5c4f5283c500f55c8d3583
ankush <ankush@compose.co.in>
2024-09-08 15:38:54 +0530

    update: social media links

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,6 +2,7 @@ import { FaLocationArrow } from 'react-icons/fa6';
 
 import { socialMedia } from '@/data';
 import MagicButton from './MagicButton';
+import Link from 'next/link';
 
 const Footer = () => {
   return (
@@ -29,14 +30,19 @@ const Footer = () => {
         </p>
 
         <div className='flex items-center md:gap-3 gap-6 mt-8'>
-          {socialMedia.map((info) => (
-            <div
-              key={info.id}
-              className='w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300'
-            >
-              <img src={info.img} alt='icons' width={20} height={20} />
-            </div>
-          ))}
+          {socialMedia.map(
+            (info) =>
+              info?.link && (
+                <Link href={info.link} target='_blank'>
+                  <div
+                    key={info.id}
+                    className='w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300'
+                  >
+                    <img src={info.img} alt='icons' width={20} height={20} />
+                  </div>
+                </Link>
+              )
+          )}
         </div>
       </div>
     </footer>
