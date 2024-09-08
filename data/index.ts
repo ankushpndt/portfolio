cc03d400201ce73aba9ddfab410ca16763dceccd
ankush <ankush@compose.co.in>
2024-09-08 15:32:52 +0530

    update: projects info and link

--- data/index.ts
+++ data/index.ts
@@ -10,7 +10,8 @@ export const gridItems = [
     id: 1,
     title: 'I prioritize client collaboration, fostering open communication ',
     description: '',
-    className: 'min-[1350px]:col-span-3 md:col-span-6 md:row-span-4 min-[1350px]:min-h-[60vh]',
+    className:
+      'min-[1350px]:col-span-3 md:col-span-6 md:row-span-4 min-[1350px]:min-h-[60vh]',
     imgClassName: 'w-full h-full',
     titleClassName: 'justify-end',
     img: '/b1.svg',
@@ -53,7 +54,8 @@ export const gridItems = [
     description: 'The Inside Scoop',
     className: 'md:col-span-3 md:row-span-2',
     imgClassName: 'absolute right-0 bottom-0 md:w-96 w-60',
-    titleClassName: 'justify-center md:justify-start min-[1350px]:justify-center',
+    titleClassName:
+      'justify-center md:justify-start min-[1350px]:justify-center',
     img: '/b5.svg',
     spareImg: '/grid.svg',
   },
@@ -72,35 +74,35 @@ export const gridItems = [
 export const projects = [
   {
     id: 1,
-    title: '3D Solar System Planets to Explore',
-    des: 'Explore the wonders of our solar system with this captivating 3D simulation of the planets using Three.js.',
+    title: 'Video Libary App',
+    des: 'Video Library web app built using MERN stack',
     img: '/p1.svg',
-    iconLists: ['/re.svg', '/tail.svg', '/ts.svg', '/three.svg', '/fm.svg'],
-    link: '/ui.earth.com',
+    iconLists: ['/mongoDB.svg', '/express.svg', '/re.svg', '/node.svg'],
+    link: 'https://ligmavideo.netlify.app/',
   },
   {
     id: 2,
-    title: 'Yoom - Video Conferencing App',
-    des: 'Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.',
+    title: 'ShopAtUP - eCommerce App',
+    des: 'Ecommerce App built using MERN stack and stripe',
     img: '/p2.svg',
-    iconLists: ['/next.svg', '/tail.svg', '/ts.svg', '/stream.svg', '/c.svg'],
-    link: '/ui.yoom.com',
+    iconLists: ['/mongoDB.svg', '/express.svg', '/re.svg', '/node.svg'],
+    link: 'https://shopatup.netlify.app/',
   },
   {
     id: 3,
-    title: 'AI Image SaaS - Canva Application',
-    des: 'A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.',
+    title: 'Ligma Social - social networking app',
+    des: 'Ligma Social App built using MERN, Redux Toolkit and Socket IO',
     img: '/p3.svg',
-    iconLists: ['/re.svg', '/tail.svg', '/ts.svg', '/three.svg', '/c.svg'],
-    link: '/ui.aiimg.com',
+    iconLists: ['/mongoDB.sv', '/express.svg', '/re.svg', '/node.svg'],
+    link: 'https://ligmasocial.netlify.app/',
   },
   {
     id: 4,
-    title: 'Animated Apple Iphone 3D Website',
-    des: 'Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..',
+    title: 'Ligma Notes',
+    des: 'Ligma Notes App built using MERN stack',
     img: '/p4.svg',
-    iconLists: ['/next.svg', '/tail.svg', '/ts.svg', '/three.svg', '/gsap.svg'],
-    link: '/ui.apple.com',
+    iconLists: ['/mongoDB.svg', '/express.svg', '/re.svg', '/node.svg'],
+    link: 'https://ligmanotes.netlify.app/',
   },
 ];
 
@@ -173,31 +175,17 @@ export const companies = [
 export const workExperience = [
   {
     id: 1,
-    title: 'Frontend Engineer Intern',
-    desc: 'Assisted in the development of a web-based platform using React.js, enhancing interactivity.',
+    title: 'Frontend Developer',
+    desc: 'Assisted in the development of a realtime chat application web-based platform using React.js, enhancing interactivity.',
     className: 'md:col-span-2',
     thumbnail: '/exp1.svg',
   },
   {
     id: 2,
-    title: 'Mobile App Dev - JSM Tech',
-    desc: 'Designed and developed mobile app for both iOS & Android platforms using React Native.',
-    className: 'md:col-span-2', // change to md:col-span-2
-    thumbnail: '/exp2.svg',
-  },
-  {
-    id: 3,
-    title: 'Freelance App Dev Project',
-    desc: 'Led the dev of a mobile app for a client, from initial concept to deployment on app stores.',
-    className: 'md:col-span-2', // change to md:col-span-2
-    thumbnail: '/exp3.svg',
-  },
-  {
-    id: 4,
-    title: 'Lead Frontend Developer',
+    title: 'Senior Frontend Developer',
     desc: 'Developed and maintained user-facing features using modern frontend technologies.',
     className: 'md:col-span-2',
-    thumbnail: '/exp4.svg',
+    thumbnail: '/exp2.svg',
   },
 ];
 
