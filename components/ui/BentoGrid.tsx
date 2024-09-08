4c224f87f013656225291ba0716d07504e36e12a
ankush <ankush@compose.co.in>
2024-09-08 14:39:12 +0530

    fix: BentoGrid layout

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -1,4 +1,4 @@
-'use client'
+'use client';
 import { useState } from 'react';
 import { IoCopyOutline } from 'react-icons/io5';
 
@@ -21,7 +21,7 @@ export const BentoGrid = ({
   return (
     <div
       className={cn(
-        'grid grid-cols-1 md:grid-cols-6 lg:grid-cols-5 md:grid-row-7 gap-4 lg:gap-8 mx-auto',
+        'grid grid-cols-1 md:grid-cols-6 min-[1350px]:grid-cols-5 md:grid-row-7 gap-4 lg:gap-8 mx-auto',
         className
       )}
     >
@@ -49,8 +49,8 @@ export const BentoGridItem = ({
   titleClassName?: string;
   spareImg?: string;
 }) => {
-  const leftLists = ['ReactJS', 'Express', 'Typescript'];
-  const rightLists = ['VueJS', 'NuxtJS', 'GraphQL'];
+  const leftLists = ['TypeScript', 'JavaScript', 'TailwindCSS'];
+  const rightLists = ['NextJS', 'ReactJS', 'CSS'];
 
   const [copied, setCopied] = useState(false);
 
@@ -106,21 +106,23 @@ export const BentoGridItem = ({
         </div>
         {id === 6 && (
           <BackgroundGradientAnimation>
-            <div className='absolute z-50 inset-0 flex items-center justify-center text-white font-bold px-4 pointer-events-none text-3xl text-center md:text-4xl lg:text-7xl'></div>
+            <div className='absolute z-50 inset-0 flex items-center justify-center text-white font-bold px-4 pointer-events-none text-3xl text-center md:text-4xl min-[1350px]:text-7xl'></div>
           </BackgroundGradientAnimation>
         )}
 
         <div
           className={cn(
             titleClassName,
-            'group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10'
+            'group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 min-[1350px]:p-10'
           )}
         >
-          <div className='font-sans font-extralight md:max-w-32 md:text-xs lg:text-base text-sm text-[#C1C2D3] z-10'>
+          <div className='font-sans font-extralight md:max-w-32 md:text-xs min-[1350px]:text-base text-sm text-[#C1C2D3] z-10'>
             {description}
           </div>
           <div
-            className={`font-sans text-lg lg:text-3xl max-w-96 font-bold z-10`}
+            className={`font-sans text-lg min-[1350px]:text-3xl max-w-96 font-bold z-10 ${
+              id === 5 && 'max-[1349px]:max-w-48'
+            }`}
           >
             {title}
           </div>
@@ -130,27 +132,27 @@ export const BentoGridItem = ({
 
           {/* Tech stack list div */}
           {id === 3 && (
-            <div className='flex gap-1 lg:gap-5 w-fit absolute -right-3 lg:-right-2'>
+            <div className='flex gap-1 min-[1350px]:gap-5 w-fit absolute -right-3 min-[1350px]:-right-2'>
               {/* tech stack lists */}
-              <div className='flex flex-col gap-3 md:gap-3 lg:gap-8'>
+              <div className='flex flex-col gap-3 md:gap-3 min-[1350px]:gap-8'>
                 {leftLists.map((item, i) => (
                   <span
                     key={i}
-                    className='lg:py-4 lg:px-3 py-2 px-3 text-xs lg:text-base opacity-50 
-                    lg:opacity-100 rounded-lg text-center bg-[#10132E]'
+                    className='min-[1350px]:py-4 min-[1350px]:px-3 py-2 px-3 text-xs min-[1350px]:text-base opacity-50 
+                    min-[1350px]:opacity-100 rounded-lg text-center bg-[#10132E]'
                   >
                     {item}
                   </span>
                 ))}
-                <span className='lg:py-4 lg:px-3 py-4 px-3  rounded-lg text-center bg-[#10132E]'></span>
+                <span className='min-[1350px]:py-4 min-[1350px]:px-3 py-4 px-3  rounded-lg text-center bg-[#10132E]'></span>
               </div>
-              <div className='flex flex-col gap-3 md:gap-3 lg:gap-8'>
-                <span className='lg:py-4 lg:px-3 py-4 px-3  rounded-lg text-center bg-[#10132E]'></span>
+              <div className='flex flex-col gap-3 md:gap-3 min-[1350px]:gap-8'>
+                <span className='min-[1350px]:py-4 min-[1350px]:px-3 py-4 px-3  rounded-lg text-center bg-[#10132E]'></span>
                 {rightLists.map((item, i) => (
                   <span
                     key={i}
-                    className='lg:py-4 lg:px-3 py-2 px-3 text-xs lg:text-base opacity-50 
-                    lg:opacity-100 rounded-lg text-center bg-[#10132E]'
+                    className='min-[1350px]:py-4 min-[1350px]:px-3 py-2 px-3 text-xs min-[1350px]:text-base opacity-50 
+                    min-[1350px]:opacity-100 rounded-lg text-center bg-[#10132E]'
                   >
                     {item}
                   </span>
